4438afbdfa5191e77229229fb9126fd6ad731f82
shihuiarenjinba-png <shihuiarenjinba@gmail.com>
2026-09-03 17:25:53 +0900

    Update Windows setup for Streamlit UI

--- excel_csv_safe_transfer/setup.bat
+++ excel_csv_safe_transfer/setup.bat
@@ -1,8 +1,8 @@
 @echo off
 chcp 65001 > nul
 cd /d "%~dp0"
-echo pywin32 をインストールします。
+echo Streamlit と Excel 処理ライブラリをインストールします。
 py -m pip install -r requirements.txt
 echo.
-echo 完了しました。
+echo 完了しました。run.bat で起動できます。
 pause
